0dd3d19325b57552ef8b0080dee0db06951b0ac3
sidharth nayyar <sidnayyar1@gmail.com>
2019-06-11 18:03:30 -0400

    classes and structure

---
+++ ClassandStructure/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  ClassandStructure
+//
+//  Created by MacStudent on 2019-06-11.
+//
+
+import Foundation
+
+var s1 = Student() // calling the constructers
+
+s1.dislay()
+s1.setValues()
+s1.dislay()
+
+s1.printData()
+
+print(s1.getFirstName())
+
+var s2: Student
+
+s2 = Student()
